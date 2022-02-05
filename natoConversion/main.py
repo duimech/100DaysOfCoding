5389a8b1a7eb2aef1382de47a4618a169b9ddf5d
Ray Bolin <duimech@gmail.com>
2022-02-05 11:08:19 -0500

    added error checking

--- natoConversion/main.py
+++ natoConversion/main.py
@@ -1,26 +1,23 @@
 # This program converts a word entered into a list of NATO words for each letter in the word.
 # Author: Ray Bolin
-# Date: 1/29/2022
+# Date: 2/5/2022
 # 100DaysOfCoding
 
 import pandas
-
 df = pandas.read_csv("100DaysOfCoding/natoConversion/nato_phonetic_alphabet.csv")
-
-# df_dictionary = {}
-# for (index,row) in df.iterrows():
-#     key = row.letter
-#     value = row.code
-#     df_dictionary[key] = value
 
 # Go through each row in the DataFrame. Add the columns "letter" and "code" to the new dictionary.
 df_dictionary = {row.letter:row.code for (index, row) in df.iterrows()}
 
-# Convert the word entered to upper case and make it a list. Make a new list to print containing the nato output.
-user_input = list(input("Enter a word:  ").upper())
-output = [df_dictionary[letter] for letter in user_input]
+def convert():
+    user_input = list(input("Enter a word:  ").upper())
+    # Make a new list to print containing the nato output from the user input.
+    try:        
+        output = [df_dictionary[letter] for letter in user_input]
+    except KeyError:
+        print("Only enter letters from the alphabet.")
+        convert()
+    else:
+        print(output)
 
-# for letter in user_input:
-#     output.append(df_dictionary[letter])
-
-print(output)
+convert()
